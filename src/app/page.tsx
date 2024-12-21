29d6fcb660caef719a1c569c965969c0fd3dad1f
Lauren Yeung <laurenyeung217@gmail.com>
2024-12-21 23:30:25 +0000

    Setup google oauth authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+import { signIn } from "@/auth";
+
 export default function Home() {
   return (
-    <div className="">test</div>
+    <div className="">
+      test<br />
+      <form
+        action={async () => {
+          "use server"
+          await signIn("google")
+        }}
+      >
+        <button type="submit" 
+            className="border px-4 py-2 rounded-lg bg-ig-red text-white">
+          Signin with Google
+        </button>
+      </form>
+    </div>
   );
 }
