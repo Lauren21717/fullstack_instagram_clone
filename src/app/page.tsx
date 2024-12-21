04bce1754d09a30cc834f01463778be732b0a17d
Lauren Yeung <laurenyeung217@gmail.com>
2024-12-21 23:37:05 +0000

    Add signout functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,36 @@
-import { signIn } from "@/auth";
+import { auth, signIn, signOut } from "@/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
   return (
     <div className="">
       test<br />
-      <form
-        action={async () => {
-          "use server"
-          await signIn("google")
-        }}
-      >
-        <button type="submit" 
-            className="border px-4 py-2 rounded-lg bg-ig-red text-white">
-          Signin with Google
-        </button>
-      </form>
+      {session && (
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+          }}
+        >
+          <button type="submit" 
+              className="border px-4 py-2 rounded-lg bg-ig-red text-white">
+            Signout
+          </button>
+        </form>
+      )}
+      {!session && (
+        <form
+          action={async () => {
+            "use server";
+            await signIn("google");
+          }}
+        >
+          <button type="submit" 
+              className="border px-4 py-2 rounded-lg bg-ig-red text-white">
+            Signin with Google
+          </button>
+        </form>
+      )}
     </div>
   );
 }
